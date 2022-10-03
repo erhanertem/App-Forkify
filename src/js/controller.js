ae9019d15fd6d0961713d28b1eba4f9701bdf0c2
Erhan Ertem <eertem@gmail.com>
2022-10-03 19:12:37 +0300

    Fetch data from forkify api and reframe the recipe data for the app

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,4 +12,29 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-console.log('TEST'); //test to see if the parcel setup is succesfull
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    console.log(response, data, data.data);
+    // let recipe = data.data.recipe;
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    }; //Lets create our own version of the recipe to be used in the app
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
