f2a1bb4725250ee18634e2c2b2c66194f97d52c4
Erhan Ertem <eertem@gmail.com>
2022-10-15 11:04:13 +0300

    Refactored the recipe bookmark assignment

--- src/js/model.js
+++ src/js/model.js
@@ -53,12 +53,14 @@ export const loadRecipe = async function (id) {
     // console.log('State recipe:', state.recipe);
 
     //-->Persist bookmarks between recipe search instances by checking recipe viewed against the registered bookmark list
-    if (state.bookmarks.some(bookmark => bookmark.id === id))
-      state.recipe.bookmarked = true;
-    else state.recipe.bookmarked = false;
-    // state.recipe.bookmarked = state.bookmarks.some(bookmark => {
-    //   bookmark.id === id;
-    // });
+    // //#1-aalternate code
+    // if (state.bookmarks.some(bookmark => bookmark.id === id))
+    //   state.recipe.bookmarked = true;
+    // else state.recipe.bookmarked = false;
+    //#2 Alternate code
+    state.recipe.bookmarked = state.bookmarks.some(
+      bookmark => bookmark.id === id
+    );
     // console.log('🎆', state.recipe.bookmarked);
     //NOTE: Some() returns a boolean value. If bookmark ids do not match returns either false or true for state.recipe.bookmarked
   } catch (err) {
